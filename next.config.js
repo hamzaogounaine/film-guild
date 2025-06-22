e712787800eb5f4f3fef6a19fc572f9e4e12bb00
hamzaogounaine <hamz2487@gmail.com>
2025-06-22 12:11:21 +0100

    next config updated

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const fs = require('fs');
 
 
-const dotenv = require('dotenv');
-
-const env = dotenv.parse(fs.readFileSync('.env'));
+const {config} = require('dotenv');
+config()
 
 const nextConfig = {
   reactStrictMode: true,
-  env: env,
   images: {
     domains: ['image.tmdb.org'], // Add the hostnames of external image URLs
   },
