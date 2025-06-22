65bdbfbd0d78c3092de4a9d5e8c6c85a732d025b
hamzaogounaine <hamz2487@gmail.com>
2025-06-22 22:38:50 +0100

    locals storage problem fixed

--- src/app/watch/[id]/page.js
+++ src/app/watch/[id]/page.js
@@ -21,6 +21,7 @@ import axios from "axios"
 import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 const Page = () => {
   const servers = [
