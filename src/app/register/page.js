c2af49112473526875aee8c7466577bcc5549eef
hamzaogounaine <hamz2487@gmail.com>
2025-06-28 19:24:33 +0100

    Watch List added with login

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -176,7 +176,7 @@ export default function Signin() {
               <div className="w-full border-t border-gray-700"></div>
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-4 bg-gray-800/50 text-gray-400">Don't have an account?</span>
+              <span className="px-4 bg-gray-800/50 text-gray-400">Don&apos;t have an account?</span>
             </div>
           </div>
 
