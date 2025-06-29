65448b70bc8aa6b570f83431bb5861a72f7dfa9f
hamzaogounaine <hamz2487@gmail.com>
2025-06-29 16:01:06 +0100

    Major changes

--- src/app/watchlist/page.js
+++ src/app/watchlist/page.js
@@ -293,7 +293,7 @@ export default function Watchlist() {
               <Search className="w-10 h-10 sm:w-12 sm:h-12 text-gray-600" />
             </div>
             <h3 className="text-xl sm:text-2xl font-bold mb-2 sm:mb-2">No results found</h3>
-            <p className="text-gray-400 text-base sm:text-lg">No items in your watchlist match "{searchQuery}"</p>
+            <p className="text-gray-400 text-base sm:text-lg">No items in your watchlist match &quot;{searchQuery}&quot;</p>
           </div>
         )}
 
